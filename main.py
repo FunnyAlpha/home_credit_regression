dea41174fe9b17f1bd9f852df535c9854821fe4b
Maxim Solovev <noguelle@gmail.com>
2020-10-23 01:53:45 +0300

    EDA starting

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-print("start")
+#%%
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+#%%
+import os
+
+PATH = "D:\projects\home_credit_regression\data"
+# print(os.listdir(PATH))
+
+# upload data
+app_train = pd.read_csv("./data/application_train.csv")
+app_test = pd.read_csv("./data/application_test.csv")
+
+#%%
+pd.set_option('display.max_columns', None)
+# print(app_train.TARGET.value_counts())
+# print(app_train.head())
+
+#%%
+#plot
+
+plt.style.use('fivethirtyeight')
+plt.rcParams["figure.figsize"] = [8,5]
+
+plt.hist(app_train.TARGET)
+
+plt.show()
